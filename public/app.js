ac1be196fd24ecd821ebfe891727ad8c39fdc118
Vitor Bortolotti Ribeiro <vitor.ribeiro@ifes.edu.br>
2018-08-26 02:46:09 -0300

    Implementando autenticacao anônima

--- public/app.js
+++ public/app.js
@@ -10,4 +10,11 @@ angular.module('app').config(function() {
         messagingSenderId: "212780034321"
     };
     firebase.initializeApp(config);
+
+    firebase.auth().signInAnonymously().catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+    });
 });
