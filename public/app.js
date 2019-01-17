bbcfa9105f030212fd0ea54966a0b1d5d6aadf2e
Vitor Bortolotti Ribeiro <vitor.ribeiro@ifes.edu.br>
2019-01-16 22:05:16 -0200

    Alterando informações na página sobre

--- public/app.js
+++ public/app.js
@@ -7,13 +7,13 @@ angular.module("app").config(function ($stateProvider, $urlRouterProvider, $loca
         $stateProvider
             .state("home", {
                 url: "/",
-                templateUrl: "home.template.html",
+                templateUrl: "home.template.html?v=1.0.0",
                 params: {},
                 resolve: {}
             })
             .state("about", {
                 url: "/sobre",
-                templateUrl: "about.template.html",
+                templateUrl: "about.template.html?v=1.0.0",
                 params: {},
                 resolve: {}
             })
